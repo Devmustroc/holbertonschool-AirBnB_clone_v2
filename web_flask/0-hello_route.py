9ac09713545c005ccdc5145de60a7cddbcbc2e69
devmustroc <@Ismail251217>
2022-09-10 15:09:03 +0200

    New

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/airbnb-onepage/', strict_slashes=False)
-def index():
+@app.route('/')
+def hello_framework():
     """ display Hello HBNB! """
     return 'Hello HBNB!'
 
@@ -13,3 +13,4 @@ def index():
 if __name__ == '__main__':
     """application must be listening on"""
     app.run(host='0.0.0.0', port='5000')
+    app.url_map.strict_slashes = False
